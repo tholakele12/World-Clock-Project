57a0ba01f6006e9c452da00af3c3e68848572d78
tholakele12 <tholakeledube36@gmail.com>
2025-05-23 14:13:57 +0100

    added codes on index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+function updateLosAngelesTime(){
+
+
+
+
+
+let losAngelesElement = document.querySelector("#los-angeles")
+let losAngelesDateElement=losAngelesElement.querySelector(".date")
+let losAngelesTimeElement=losAngelesElement.querySelector(".time")
+let losAngelesTime=moment().tz("America/los_Angeles")
+
+
+losAngelesDateElement.innerHTML= losAngelesTime.format("MMMM Do YYYY")
+losAngelesTimeElement.innerHTML=`${losAngelesTime.format("h:mm:ss SS")} <small>${losAngelesTime.format("A")}</small>`
+
+}
+updateLosAngelesTime()
+setInterval(updateLosAngelesTime, 1000)
+
+//Paris//
+function updateParisTime() {
+
+let parisElement = document.querySelector("#paris")
+let parisDateElement=parisElement.querySelector(".date")
+let parisTimeElement=parisElement.querySelector(".time")
+let parisTime=moment().tz("Europea/paris")
+
+
+parisDateElement.innerHTML= parisTime.format("MMMM Do YYYY")
+parisTimeElement.innerHTML=`${parisTime.format("h:mm:ss SS")} <small>${parisTime.format("A")}</small>`
+}
+updateParisTime()
+setInterval(updateParisTime, 1000)
