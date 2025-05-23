744a799be73ccad6031400079e81074eb539605c
tholakele12 <tholakeledube36@gmail.com>
2025-05-23 15:22:27 +0100

    Added current location

--- index.js
+++ index.js
@@ -35,6 +35,9 @@ setInterval(updateParisTime, 1000)
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+        cityTimeZone=moment.tz.guess()
+    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
